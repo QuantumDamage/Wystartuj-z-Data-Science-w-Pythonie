4f72842c2f2d1bb8233ac89034d80bf99338b307
Damian Melniczuk <damian.melniczuk@gmail.com>
2018-07-06 16:17:11 +0200

    aktualizacja

--- nowotwor/output/tpot.py
+++ nowotwor/output/tpot.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import StandardScaler
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,11 +12,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.9780219780219781
+# Score on the training set was:0.9846153846153847
 exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_classif, percentile=89),
-    MinMaxScaler(),
-    LogisticRegression(C=10.0, dual=True, penalty="l2")
+    StackingEstimator(estimator=LogisticRegression(C=0.0001, dual=False, penalty="l1")),
+    StandardScaler(),
+    LogisticRegression(C=0.1, dual=False, penalty="l2")
 )
 
 exported_pipeline.fit(training_features, training_target)
